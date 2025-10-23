495f5778de515c1e77d7068e0db90509fddb69e6
hammad-ibrahim <95659983+hammad-ibrahim@users.noreply.github.com>
2025-10-23 03:14:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-
-m flask import Flask, render_template, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-        model = {"title": " Hello Build Trigger ."}
-            return render_template('index.html', model=model)
+    model = {"title": "Hello Build Trigger."}
+    return render_template('index.html', model=model)
 
-        if __name__ == "__main__":
-                app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
-
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
