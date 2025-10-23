31fe23bdec5909e8007a837b1049e540ddb2ae4d
hammad-ibrahim <95659983+hammad-ibrahim@users.noreply.github.com>
2025-10-23 03:47:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
+# إنشاء التطبيق
 app = Flask(__name__)
 
+# الراوت الرئيسي
 @app.route("/")
 def main():
     model = {"title": "Hello Build Trigger."}
-    return render_template('index.html', model=model)
+    return render_template("index.html", model=model)
 
+# تشغيل التطبيق لو الملف اتشغّل مباشرة
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
